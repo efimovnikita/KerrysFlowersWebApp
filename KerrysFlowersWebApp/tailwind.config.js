f079e3e5ca2be7f0508f0b3235a8f8717c221468
maskedball <maskedballmail@gmail.com>
2022-09-30 17:53:17 +0300

    spin up blazor

---
+++ KerrysFlowersWebApp/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{razor,cshtml,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto Slab', serif",
+        montserrat: " 'Montserrat', sans-serif",
+      },
+      colors: {
+        background: "#292524",
+        text1: "#44403C",
+        text2: "#FAFAF9",
+        menu: "#1C1917",
+        menutext: "#FDD9C2",
+        heading: "#FDC6A2",
+        subheading: "#EAB896",
+        card: "#E2B4DE",
+        secondaryaccent: "#6F296B",
+        label: "#B3090A",
+      },
+    },
+  },
+  plugins: [],
+};
