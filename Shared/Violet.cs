810d04020b11895870b5a1b8d1bb6b54914b01f6
maskedball <maskedballmail@gmail.com>
2022-10-01 10:32:48 +0300

    add violet maker proj

--- Shared/Violet.cs
+++ Shared/Violet.cs
@@ -3,11 +3,24 @@ namespace Shared;
 [Serializable]
 public class Violet
 {
+    public Violet(string name, string breeder, string description, List<string> tags, bool newLabel, DateTime date,
+        List<Image> images)
+    {
+        Name = name;
+        Breeder = breeder;
+        Description = description;
+        Tags = tags;
+        NewLabel = newLabel;
+        Date = date;
+        Images = images;
+    }
+
+    public Guid Id { get; private set; } = Guid.NewGuid();
     public string Name { get; set; }
     public string Breeder { get; set; }
     public string Description { get; set; }
     public List<string> Tags { get; set; }
-    public bool New { get; set; }
-    public List<Image> Images { get; set; } = new(3);
+    public bool NewLabel { get; set; }
+    public List<Image> Images { get; set; }
     public DateTime Date { get; set; }
 }
