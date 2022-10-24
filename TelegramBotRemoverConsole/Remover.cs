3c61efe08fe99593d1a17fb84ebcafc6a7c114cc
Nikita Efimov <maskedballmail@gmail.com>
2022-10-24 12:47:40 +0300

    change execute async

--- TelegramBotRemoverConsole/Remover.cs
+++ TelegramBotRemoverConsole/Remover.cs
@@ -143,12 +143,11 @@ internal class Remover : BackgroundService
             replyMarkup: inlineKeyboard);
     }
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         TelegramBotClient client = new(_apiKey);
         client.StartReceiving(UpdateHandler, PollingErrorHandler);
             
         Console.ReadLine();
-        return Task.CompletedTask;
     }
 }
