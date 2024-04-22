9429ec5997527098f8897b426855420354b9da16
Nikita Efimov <maskedballmail@gmail.com>
2024-04-22 20:57:26 +0300

    Add violet size enums

---
+++ SharedLibrary/VioletSize.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace SharedLibrary;
+
+public enum VioletSize
+{
+    [Description("Мини")]
+    Mini, 
+
+    [Description("Полумини")]
+    Midi, 
+
+    [Description("Стандарт")]
+    Standard, 
+
+    [Description("Малый стандарт")]
+    SmallStandard
+}
