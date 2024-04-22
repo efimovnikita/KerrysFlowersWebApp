566a7ec84a69316c880a8c3ebecd1073c607fe1d
Nikita Efimov <maskedballmail@gmail.com>
2024-04-22 18:16:16 +0300

    Add database support for frontend

--- KerrysFlowersWebApp/Program.cs
+++ KerrysFlowersWebApp/Program.cs
@@ -1,11 +1,43 @@
 using ComponentsLibrary.Services;
+using SharedLibrary.Providers;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+var dbAdmin = Environment.GetEnvironmentVariable("DBADMIN");
+if (string.IsNullOrEmpty(dbAdmin))
+{
+    throw new ArgumentNullException(nameof(dbAdmin), "Environment variable DBADMIN is not set.");
+}
+
+var dbPassword = Environment.GetEnvironmentVariable("DBPASSWORD");
+if (string.IsNullOrEmpty(dbPassword))
+{
+    throw new ArgumentNullException(nameof(dbPassword), "Environment variable DBPASSWORD is not set.");
+}
+        
+var dbHost = Environment.GetEnvironmentVariable("DBHOST");
+if (string.IsNullOrEmpty(dbHost))
+{
+    throw new ArgumentNullException(nameof(dbHost), "Environment variable DBHOST is not set.");
+}
+        
+var dbPort = Environment.GetEnvironmentVariable("DBPORT");
+if (string.IsNullOrEmpty(dbPort))
+{
+    throw new ArgumentNullException(nameof(dbPort), "Environment variable DBPORT is not set.");
+}
+        
+var dbDatabase = Environment.GetEnvironmentVariable("DBDATABASE");
+if (string.IsNullOrEmpty(dbDatabase))
+{
+    throw new ArgumentNullException(nameof(dbDatabase), "Environment variable DBDATABASE is not set.");
+}
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<ClipboardService>();
+builder.Services.AddSingleton<IVioletRepository>(_ => new VioletRepository($"mongodb://{dbAdmin}:{dbPassword}@{dbHost}:{dbPort}", dbDatabase));
 
 WebApplication app = builder.Build();
 
