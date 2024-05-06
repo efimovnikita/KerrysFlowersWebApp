c5752a247a73d5020df3060022ccfa309ff0a1be
Nikita Efimov <maskedballmail@gmail.com>
2024-05-06 08:42:10 +0300

    Fix StoredViolet.cs

--- SharedLibrary/StoredViolet.cs
+++ SharedLibrary/StoredViolet.cs
@@ -1,13 +1,17 @@
 ﻿namespace SharedLibrary;
 
-public class StoredViolet(Violet violet, WarehouseVioletItem warehouseItem) : Violet(violet.Id, violet.Name,
+public class StoredViolet(Violet violet, WarehouseVioletItem warehouseItem = null) : Violet(violet.Id, violet.Name,
     violet.Breeder, violet.Description, violet.Tags,
     violet.BreedingDate, violet.Images, violet.IsChimera, violet.Colors, violet.Size)
 {
-    public int LeafCount { get; set; } = warehouseItem.LeafCount;
-    public double LeafPrice { get; set; } = warehouseItem.LeafPrice;
-    public int ChildCount { get; set; } = warehouseItem.ChildCount;
-    public double ChildPrice { get; set; } = warehouseItem.ChildPrice;
-    public int WholePlantCount { get; set; } = warehouseItem.WholePlantCount;
-    public double WholePlantPrice { get; set; } = warehouseItem.WholePlantPrice;
+    private const int DefaultCount = 0;
+    private const double DefaultPrice = 0.0;
+
+    public int LeafCount { get; set; } = warehouseItem?.LeafCount ?? DefaultCount;
+    public double LeafPrice { get; set; } = warehouseItem?.LeafPrice ?? DefaultPrice;
+    public int ChildCount { get; set; } = warehouseItem?.ChildCount ?? DefaultCount;
+    public double ChildPrice { get; set; } = warehouseItem?.ChildPrice ?? DefaultPrice;
+    public int WholePlantCount { get; set; } = warehouseItem?.WholePlantCount ?? DefaultCount;
+    public double WholePlantPrice { get; set; } = warehouseItem?.WholePlantPrice ?? DefaultPrice;
 }
+
