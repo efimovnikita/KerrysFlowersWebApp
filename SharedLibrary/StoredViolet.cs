3e3ddac23782beff1b1259fa4e2eecd6ed40e7c8
Nikita Efimov <maskedballmail@gmail.com>
2024-05-08 16:32:42 +0300

    Added methods for StoredViolet.cs

--- SharedLibrary/StoredViolet.cs
+++ SharedLibrary/StoredViolet.cs
@@ -7,16 +7,85 @@ public class StoredViolet(Violet violet, WarehouseVioletItem warehouseItem = nul
     private const int DefaultCount = 0;
     private const double DefaultPrice = 0.0;
 
-    public int LeafCount { get; set; } = warehouseItem?.LeafCount ?? DefaultCount;
-    public double LeafPrice { get; set; } = warehouseItem?.LeafPrice ?? DefaultPrice;
-    public int ChildCount { get; set; } = warehouseItem?.ChildCount ?? DefaultCount;
-    public double ChildPrice { get; set; } = warehouseItem?.ChildPrice ?? DefaultPrice;
-    public int WholePlantCount { get; set; } = warehouseItem?.WholePlantCount ?? DefaultCount;
-    public double WholePlantPrice { get; set; } = warehouseItem?.WholePlantPrice ?? DefaultPrice;
+    private int LeafCount { get; set; } = warehouseItem?.LeafCount ?? DefaultCount;
+    private double LeafPrice { get; set; } = warehouseItem?.LeafPrice ?? DefaultPrice;
+    private int SelectedLeafs { get; set; }
+    private int ChildCount { get; set; } = warehouseItem?.ChildCount ?? DefaultCount;
+    private double ChildPrice { get; set; } = warehouseItem?.ChildPrice ?? DefaultPrice;
+    private int SelectedChildren { get; set; }
+    private int WholePlantCount { get; set; } = warehouseItem?.WholePlantCount ?? DefaultCount;
+    private double WholePlantPrice { get; set; } = warehouseItem?.WholePlantPrice ?? DefaultPrice;
+    private int SelectedWholePlants { get; set; }
 
     public bool IsLeafsInfoValid() => LeafCount != 0 && LeafPrice != 0.0;
     public bool IsChildrenInfoValid() => ChildCount != 0 && ChildPrice != 0.0;
     public bool IsWholePlantInfoValid() => WholePlantCount != 0 && WholePlantPrice != 0.0;
     public bool IsWarehouseInfoValid() => IsLeafsInfoValid() || IsChildrenInfoValid() || IsWholePlantInfoValid();
+
+    public double CalcSelectedLeafsPrice() => SelectedLeafs * LeafCount;
+    public double CalcSelectedChildrenPrice() => SelectedChildren * ChildPrice;
+    public double CalcSelectedWholePlantsPrice() => SelectedWholePlants * WholePlantPrice;
+
+    public double CalcTotalPriceForViolet() =>
+        CalcSelectedLeafsPrice() + CalcSelectedChildrenPrice() + CalcSelectedWholePlantsPrice();
+    public void IncreaseNumberOfSelectedLeafs()
+    {
+        if (SelectedLeafs + 1 > LeafCount)
+        {
+            return;
+        }
+
+        SelectedLeafs += 1;
+    }
+    
+    public void DecreaseNumberOfSelectedLeafs()
+    {
+        if (SelectedLeafs == 0)
+        {
+            return;
+        }
+
+        SelectedLeafs -= 1;
+    }
+    
+    public void IncreaseNumberOfSelectedChildren()
+    {
+        if (SelectedChildren + 1 > ChildCount)
+        {
+            return;
+        }
+
+        SelectedChildren += 1;
+    }
+    
+    public void DecreaseNumberOfSelectedChildren()
+    {
+        if (SelectedChildren == 0)
+        {
+            return;
+        }
+
+        SelectedChildren -= 1;
+    }
+    
+    public void IncreaseNumberOfSelectedWholePlants()
+    {
+        if (SelectedWholePlants + 1 > WholePlantCount)
+        {
+            return;
+        }
+
+        SelectedWholePlants += 1;
+    }
+    
+    public void DecreaseNumberOfSelectedWholePlants()
+    {
+        if (SelectedWholePlants == 0)
+        {
+            return;
+        }
+
+        SelectedWholePlants -= 1;
+    }
 }
 
